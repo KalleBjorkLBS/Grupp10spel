de96802acad038eaccfe59ee29f2e99e97a2a7d7
KalleBjorkLBS <56306956+KalleBjorkLBS@users.noreply.github.com>
2020-01-08 14:14:04 +0100

    Player, gun control

---
+++ Grupp10Spel/Assets/Gun.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+
+    [SerializeField]
+    GameObject target = null;
+
+    public float speed = 2f;
+
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        Vector3 vectorToTarget = target.transform.position - transform.position;
+        float angle = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg;
+        Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = Quaternion.Slerp(transform.rotation, q, Time.deltaTime * speed);
+    }
+}
---
+++ Grupp10Spel/Assets/Scripts/Player.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    Rigidbody2D rb = null;
+
+    public float fallMultiplier = 2.5f;
+    public float lowJumpMultiplier = 2f;
+    public float jumpMultiplier = 100f;
+
+
+    private bool isGrounded = false;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded == true)
+        {
+            rb.AddForce(new Vector2(0, 10 * jumpMultiplier));
+            isGrounded = false;
+        }
+
+
+        if (rb.velocity.y <0)
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        } else if(rb.velocity.y > 0 && !Input.GetKey(KeyCode.Space))
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }    
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "mark")
+        {
+            isGrounded = true;
+        }
+    }
+
+
+}
