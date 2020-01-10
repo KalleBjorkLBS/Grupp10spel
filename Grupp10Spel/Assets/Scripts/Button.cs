23f4ea0c0e8e21fbb609c082c355adb6000ccc3b
KalleBjorkLBS <56306956+KalleBjorkLBS@users.noreply.github.com>
2020-01-10 13:18:06 +0100

    Splash Scene

---
+++ Grupp10Spel/Assets/Scripts/Button.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Button : MonoBehaviour
+{
+    void Update()
+    {
+        
+    }
+
+    public void ExitOnClick()
+    {
+        Application.Quit();
+    }
+    public void StartOnClick()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
