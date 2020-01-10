23add5e372904b2348b36cb2258ff90756a31c92
KalleBjorkLBS <56306956+KalleBjorkLBS@users.noreply.github.com>
2020-01-10 13:40:14 +0100

    Update Button.cs

--- Grupp10Spel/Assets/Scripts/Button.cs
+++ Grupp10Spel/Assets/Scripts/Button.cs
@@ -4,11 +4,6 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class Button : MonoBehaviour
 {
-    void Update()
-    {
-        
-    }
-
     public void ExitOnClick()
     {
         Application.Quit();
