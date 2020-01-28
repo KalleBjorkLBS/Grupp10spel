2c7e0c80493509bc521dc7fc9dcf8f7d78ee34e4
KalleBjorkLBS <56306956+KalleBjorkLBS@users.noreply.github.com>
2020-01-28 13:59:22 +0100

    Update Player.cs

--- Grupp10Spel/Assets/Scripts/Player.cs
+++ Grupp10Spel/Assets/Scripts/Player.cs
@@ -63,8 +63,8 @@ void Update()
 
         if (sceneId == 1)
         {
-            fallMultiplier = 5;
-            lowJumpMultiplier = 5;
+            fallMultiplier = 4.5f;
+            lowJumpMultiplier = 4.5f;
 
             shotPower = 25;
         }
@@ -74,8 +74,6 @@ void Update()
             fallMultiplier = 1;
             lowJumpMultiplier = 1;
         }
-
-        print(sceneId);
 
         if (Input.GetKeyDown(KeyCode.G))
         {
