2a98dbe0b2afff76f88e4cf608df19d239d1b852
Jsid03 <56590042+Jsid03@users.noreply.github.com>
2020-03-21 19:00:27 +0100

    Update Parallax.cs

--- Grupp10Spel/Assets/Scripts/Parallax.cs
+++ Grupp10Spel/Assets/Scripts/Parallax.cs
@@ -9,20 +9,26 @@ public class Parallax : MonoBehaviour
     public float parallaxEffect;
     void Start()
     {
-        startpos = transform.position.x;
-        length = GetComponent<SpriteRenderer>().bounds.size.x;
+        startpos = transform.position.x; //Hittar startpositionen på sprite
+        length = GetComponent<SpriteRenderer>().bounds.size.x; //Hittar storleken på spriten
         
     }
 
    
-    void Update()
+    void Update() //Flyttar på spriten relativt till camerapositionen och flyttar spriten till mitten av kameran när den har rört sig mer än dess längd 
     {
         float temp = (cam.transform.position.x * (1 - parallaxEffect));
         float dist = (cam.transform.position.x * parallaxEffect);
 
         transform.position = new Vector3(startpos + dist, transform.position.y , transform.position.z);
 
-        if (temp > startpos + length) startpos += length;
-        else if (temp < startpos - length) startpos -= length;
+        if (temp > startpos + length)
+        {
+            startpos += length;
+        }
+        else if (temp < startpos - length)
+        {
+            startpos -= length;
+        }
     }
 }
