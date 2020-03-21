1e16c87273a142be956e74a5205634ff25b87b8d
KalleBjorkLBS <56306956+KalleBjorkLBS@users.noreply.github.com>
2020-03-21 19:32:56 +0100

    Meny system

---
+++ Grupp10Spel/Assets/Scripts/PauseMeny.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PauseMeny : MonoBehaviour
+{
+
+    public GameObject pauseScreen = null;
+    private bool isPaused = false;
+
+    private int sceneID;
+
+    void Update()
+    {
+        sceneID = SceneManager.GetActiveScene().buildIndex;
+
+        if(Input.GetKeyDown(KeyCode.Escape)){
+
+            if(isPaused == true){
+                Resume();
+            }else{
+                Pause();
+            } 
+        }
+    }
+    public void Resume(){
+        pauseScreen.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+
+    }
+    void Pause(){
+        pauseScreen.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void Restart(){
+        SceneManager.LoadScene(sceneID);
+        Time.timeScale = 1f;
+    }
+
+    public void Quit(){
+        Application.Quit();
+    }
+}
